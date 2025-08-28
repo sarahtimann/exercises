5d7caae2a01f8c2387a67458246bb68113d2d756
Sarah Timann <saol0003@stud.kea.dk>
2025-08-28 16:16:48 +0200

    udført flere øvelser

--- exercise/test.js
+++ exercise/test.js
@@ -5,7 +5,25 @@
 
 // sayHello("Sarah");
 
-presentPet("Sarah", "dog", "Mona");
-function presentPet(firstName, animalType, animalName) {
-  console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}.`);
+// presentPet("Sarah", "dog", "Mona");
+// function presentPet(firstName, animalType, animalName) {
+//   console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}.`);
+// }
+
+function greeting(firstName) {
+  return `Hello ${firstName}`;
 }
+function lengthOfStr(str) {
+  return str.length;
+}
+console.log(lengthOfStr(greeting("Sarah")));
+
+// const greetingToSarah = greeting("Sarah");
+
+// function greeting(firstName) {
+//   return `Hello ${firstName}`;
+// }
+// const result = greeting("any name");
+// console.log(greeting("any name"));
+
+// const textContent = `Greeting is ${greeting("a name")}`;
