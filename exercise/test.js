78a056299b7bfd2744b770c1654933e2eece3f56
Sarah Timann <saol0003@stud.kea.dk>
2025-08-28 08:25:02 +0200

    lavet øvelser

---
+++ exercise/test.js
@@ -0,0 +1,11 @@
+// sayHello("Sarah");
+// function sayHello(myName) {
+//   console.log(`Hello ${myName}`);
+// }
+
+// sayHello("Sarah");
+
+presentPet("Sarah", "dog", "Mona");
+function presentPet(firstName, animalType, animalName) {
+  console.log(`My name is ${firstName}, I have a ${animalType} called ${animalName}.`);
+}
