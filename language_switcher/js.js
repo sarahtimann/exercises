a836f64eb893173b755b0b9321f4c3da63bdaad0
Sarah Timann <saol0003@stud.kea.dk>
2025-09-04 10:31:33 +0200

    version 1 virker

--- language_switcher/js.js
+++ language_switcher/js.js
@@ -13,9 +13,42 @@ const texts = {
     ],
   },
 };
-const locale = "da";
+let locale = "da";
+// const localeDe = "de";
 
-console.log(texts["de"]);
+// console.log(texts["de"]);
+
+// const qsl = "header";
+// document.querySelector(qsl).textContent = "Mona";
+
 texts[locale].texts.forEach((obj) => {
   document.querySelector(obj.location).textContent = obj.text;
 });
+
+function switchLanguage(newLocale) {
+  locale = newLocale;
+
+  texts[locale].texts.forEach((obj) => {
+    document.querySelector(obj.location).textContent = obj.text;
+  });
+}
+
+document.getElementById("de").addEventListener("click", () => switchLanguage("de"));
+
+// texts[localeDe].texts.forEach((obj) => {
+//   document.querySelector(obj.location).textContent = obj.text;
+// });
+
+// const btnDa = document.querySelector(".da");
+// const btnDe = document.querySelector(".de");
+
+// selectElement.addEventListener("change", (event) => {
+//   const selectedLanguage = event.target.value;
+//   // Update the locale variable
+//   locale = selectedLanguage;
+
+//   // Update the text content based on the selected language
+//   texts[locale].texts.forEach((obj) => {
+//     document.querySelector(obj.location).textContent = obj.text;
+//   });
+// });
