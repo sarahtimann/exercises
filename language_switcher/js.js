aa1094fa1817059bfeb69dce86a990c515e3a050
Sarah Timann <saol0003@stud.kea.dk>
2025-09-04 10:05:21 +0200

    tilføjet opgaven language_switcher

---
+++ language_switcher/js.js
@@ -0,0 +1,21 @@
+"use strict";
+const texts = {
+  de: {
+    texts: [
+      { text: "Das Bot", location: ".header" },
+      { text: "Das Ro-Bot", location: ".footer" },
+    ],
+  },
+  da: {
+    texts: [
+      { text: "Båden", location: ".header" },
+      { text: "Robotten", location: ".footer" },
+    ],
+  },
+};
+const locale = "da";
+
+console.log(texts["de"]);
+texts[locale].texts.forEach((obj) => {
+  document.querySelector(obj.location).textContent = obj.text;
+});
