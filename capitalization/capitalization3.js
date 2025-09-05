52eb3fd71446f2a39c2492433a3f819e3fa3973c
Sarah Timann <saol0003@stud.kea.dk>
2025-09-05 10:56:20 +0200

    capitalization øvelse færdig

---
+++ capitalization/capitalization3.js
@@ -0,0 +1,35 @@
+"use strict";
+
+let name = "Peter";
+let newName = "peTer";
+
+// Lav om til små bogstaver
+console.log(newName.toLowerCase()); // peter
+
+// Lav om til store bogstaver
+console.log(newName.toUpperCase()); // PETER
+
+// Første bogstav lille, næste to store, resten små
+console.log(
+  newName.substring(0, 1).toLowerCase() + // p
+    newName.substring(1, 3).toUpperCase() + // ET
+    newName.substring(3).toLowerCase() // er
+); // pETer
+
+// De to første bogstaver store, resten små
+console.log(
+  newName.substring(0, 2).toUpperCase() + // PE
+    newName.substring(2).toLowerCase() // ter
+); // PEter
+
+// Kun sidste bogstav stort, resten små
+console.log(
+  newName.slice(0, -1).toLowerCase() + // pete
+    newName.slice(-1).toUpperCase() // R
+); // peteR
+
+console.log(
+  newName.substring(0, 2).toUpperCase() + // PE
+    newName.substring(2, 3).toLowerCase() + // t
+    newName.substring(3).toUpperCase() // ER
+); // PEtER
