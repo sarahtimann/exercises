ffd3eaa817ff7d4b451e5c3499d2571ea945558b
Sarah Timann <saol0003@stud.kea.dk>
2025-09-05 11:05:29 +0200

    real capitalization øvelse færdig

---
+++ real_capitalization/capitalization.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let name = "Sarah";
+let newName = "susanne";
+
+// Lav om til små bogstaver
+console.log(newName.toLowerCase());
+
+// Lav om til store bogstaver
+console.log(newName.toUpperCase());
+
+console.log(newName[0].toUpperCase() + newName.substring(1).toLowerCase());
