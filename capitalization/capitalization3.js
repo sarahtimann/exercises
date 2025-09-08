3861df22e01f20e236f83c3a8ee73c0a6879f5a6
Sarah Timann <saol0003@stud.kea.dk>
2025-09-08 12:37:10 +0200

    gennemgang af øvelser (andre måder at løse øvelserne på)

--- capitalization/capitalization3.js
+++ capitalization/capitalization3.js
@@ -1,35 +1,42 @@
 "use strict";
 
-let name = "Peter";
-let newName = "peTer";
+const theName = "vAldeMAR";
+const threeCapitalised1 = theName.substring(0, 2).toLowerCase();
+const threeCapitalised2 = theName.substring(2, 3).toUpperCase();
+const threeCapitalised3 = theName.substring(3).toLowerCase();
 
-// Lav om til små bogstaver
-console.log(newName.toLowerCase()); // peter
+console.log(threeCapitalised1 + threeCapitalised2 + threeCapitalised3); // haNse
 
-// Lav om til store bogstaver
-console.log(newName.toUpperCase()); // PETER
+// let name = "Peter";
+// let newName = "peTer";
 
-// Første bogstav lille, næste to store, resten små
-console.log(
-  newName.substring(0, 1).toLowerCase() + // p
-    newName.substring(1, 3).toUpperCase() + // ET
-    newName.substring(3).toLowerCase() // er
-); // pETer
+// // Lav om til små bogstaver
+// console.log(newName.toLowerCase()); // peter
 
-// De to første bogstaver store, resten små
-console.log(
-  newName.substring(0, 2).toUpperCase() + // PE
-    newName.substring(2).toLowerCase() // ter
-); // PEter
+// // Lav om til store bogstaver
+// console.log(newName.toUpperCase()); // PETER
 
-// Kun sidste bogstav stort, resten små
-console.log(
-  newName.slice(0, -1).toLowerCase() + // pete
-    newName.slice(-1).toUpperCase() // R
-); // peteR
+// // Første bogstav lille, næste to store, resten små
+// console.log(
+//   newName.substring(0, 1).toLowerCase() + // p
+//     newName.substring(1, 3).toUpperCase() + // ET
+//     newName.substring(3).toLowerCase() // er
+// ); // pETer
 
-console.log(
-  newName.substring(0, 2).toUpperCase() + // PE
-    newName.substring(2, 3).toLowerCase() + // t
-    newName.substring(3).toUpperCase() // ER
-); // PEtER
+// // De to første bogstaver store, resten små
+// console.log(
+//   newName.substring(0, 2).toUpperCase() + // PE
+//     newName.substring(2).toLowerCase() // ter
+// ); // PEter
+
+// // Kun sidste bogstav stort, resten små
+// console.log(
+//   newName.slice(0, -1).toLowerCase() + // pete
+//     newName.slice(-1).toUpperCase() // R
+// ); // peteR
+
+// console.log(
+//   newName.substring(0, 2).toUpperCase() + // PE
+//     newName.substring(2, 3).toLowerCase() + // t
+//     newName.substring(3).toUpperCase() // ER
+// ); // PEtER
