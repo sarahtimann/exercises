66baaa10aab40d46b489b47ea71018ed824ecb84
Sarah Timann <saol0003@stud.kea.dk>
2025-09-09 19:58:19 +0200

    tilføjet sortby css class

--- animalbase/animalbase.js
+++ animalbase/animalbase.js
@@ -97,6 +97,13 @@ function selectSort(event) {
   const sortBy = event.target.dataset.sort;
   const sortDir = event.target.dataset.sortDirection;
 
+  // find "old" sortby element and remove .sortby class
+  const oldElement = document.querySelector(`[data-sort='${settings.sortBy}']`);
+  oldElement.classList.remove("sortby");
+
+  // indicate active sort
+  event.target.classList.add("sortby");
+
   // toggle the direction!
   if (sortDir === "asc") {
     event.target.dataset.sortDirection = "desc";
