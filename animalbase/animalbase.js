ab66f69c2c1eb14d2b50326f34c6132118786a9c
Sarah Timann <saol0003@stud.kea.dk>
2025-09-09 18:18:02 +0200

    tilføjet sorting med user interactivity

--- animalbase/animalbase.js
+++ animalbase/animalbase.js
@@ -23,6 +23,8 @@ function start() {
 
 function registerButtons() {
   document.querySelectorAll("[data-action='filter']").forEach((button) => button.addEventListener("click", selectFilter));
+
+  document.querySelectorAll("[data-action='sort']").forEach((button) => button.addEventListener("click", selectSort));
 }
 
 async function loadJSON() {
@@ -78,6 +80,39 @@ function isCat(animal) {
 
 function isDog(animal) {
   return animal.type === "dog";
+}
+
+function selectSort(event) {
+  const sortBy = event.target.dataset.sort;
+  console.log(`User selected: ${sortBy}`);
+  sortList(sortBy);
+}
+
+function sortList(sortBy) {
+  let sortedList = allAnimals;
+
+  if (sortBy === "name") {
+    sortedList = sortedList.sort(sortByName);
+  } else if (sortBy === "type") {
+    sortedList = sortedList.sort(sortByType);
+  }
+  displayList(sortedList);
+}
+
+function sortByName(animalA, animalB) {
+  if (animalA.name < animalB.name) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+
+function sortByType(animalA, animalB) {
+  if (animalA.type < animalB.type) {
+    return -1;
+  } else {
+    return 1;
+  }
 }
 
 function displayList(animals) {
