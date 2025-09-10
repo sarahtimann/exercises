5879ddb0247eeefe74b17030b1ea2cffd962fcbe
Sarah Timann <saol0003@stud.kea.dk>
2025-09-10 16:14:23 +0200

    tilføjet stjerner til model, view og controller

--- animalbase/animalbase.js
+++ animalbase/animalbase.js
@@ -10,6 +10,7 @@ const Animal = {
   desc: "-unknown animal-",
   type: "",
   age: 0,
+  star: false,
 };
 
 const settings = {
@@ -44,8 +45,8 @@ function prepareObjects(jsonData) {
   allAnimals = jsonData.map(prepareObject);
   console.log("allAnimals", allAnimals);
 
-  // TODO: This might not be the function we want to call first
-  displayList(allAnimals);
+  // fixed so we filter and sort on the first load
+  buildList();
 }
 
 function prepareObject(jsonObject) {
@@ -175,6 +176,24 @@ function displayAnimal(animal) {
   clone.querySelector("[data-field=desc]").textContent = animal.desc;
   clone.querySelector("[data-field=type]").textContent = animal.type;
   clone.querySelector("[data-field=age]").textContent = animal.age;
+
+  if (animal.star === true) {
+    clone.querySelector("[data-field=star]").textContent = "★";
+  } else {
+    clone.querySelector("[data-field=star]").textContent = "☆";
+  }
+
+  clone.querySelector("[data-field=star]").addEventListener("click", clickStar);
+
+  function clickStar() {
+    if (animal.star === true) {
+      animal.star = false;
+    } else {
+      animal.star = true;
+    }
+
+    buildList();
+  }
 
   // append clone to list
   document.querySelector("#list tbody").appendChild(clone);
