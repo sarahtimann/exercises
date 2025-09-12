e2ae584ed15583490f2d9153b8301676b951d291
Sarah Timann <saol0003@stud.kea.dk>
2025-09-12 09:51:01 +0200

    localStorage virker på theme switcher

--- theme_switcher/index.js
+++ theme_switcher/index.js
@@ -1,15 +1,27 @@
 "use strict";
 
+const storedTheme = localStorage.getItem("storedTheme");
+console.log("storedTheme", storedTheme);
+
+if (storedTheme === null) {
+  localStorage.setItem("storedTheme", "light");
+} else {
+  document.body.dataset.filter = storedTheme;
+  const p = document.querySelector("p");
+  p.dataset.filter = storedTheme;
+}
+
 const themeSwitcher = document.getElementById("theme-switcher");
 const p = document.querySelector("p");
 
-// Default tema når siden åbnes for første gang
-document.body.dataset.filter = "light";
-p.dataset.filter = "light";
-
-themeSwitcher.value = "light"; // Sæt select til light
+// // Default tema når siden åbnes for første gang
+// document.body.dataset.filter = "light";
+// p.dataset.filter = "light";
+// themeSwitcher.value = "light"; // Sæt select til light
 
 themeSwitcher.addEventListener("change", (event) => {
+  console.log("themeSwitcher", themeSwitcher);
+  localStorage.setItem("storedTheme", event.target.value);
   document.body.dataset.filter = event.target.value;
   p.dataset.filter = event.target.value;
 });
